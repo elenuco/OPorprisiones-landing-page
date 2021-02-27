e733c70cb18d59293e417936b1b5566cb017de63
Evelyn Elena Campos <47298441+elenuco@users.noreply.github.com>
2021-02-27 01:17:13 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="row">
+        <h1>OPorprisiones</h1>
+        <h2>Start editing to see some magic happen!</h2>
+      </div>
+    </div>
+  );
+}
